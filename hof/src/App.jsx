eb501060f796703f088a9b57daa94299c4821b5f
MdFazilGITHUB <mjam7221jmi@gmail.com>
2023-12-28 14:39:07 +0530

    Commit

---
+++ hof/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import HigherOrderComponent from "./components/HigherOrderComponent";
+
+export default function App() {
+  return (
+    <>
+      <div className="App">
+        <HigherOrderComponent/>
+      </div>
+    </>
+  );
+}
